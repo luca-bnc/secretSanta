62e83790b7c708190f8aa4639828fce36df6e499
luca-bnc <luca.bnc0@gmail.com>
2019-03-15 15:35:59 +0100

    changed code and solved infinite loop bug

--- santa.py
+++ santa.py
@@ -1,6 +1,6 @@
 import random
 
-people = [   #list of all people
+people = [   # list of all people
     'luca',
     'elia',
     'camilla',
@@ -20,21 +20,27 @@ people = [   #list of all people
 ]
 hasGiven, hasReceived, pairs = [], [], [] # empty lists
 i=1                # cycle counter for logging
-numberOfTries = [] #debug
 
-while (len(hasGiven)!=len(people)): # loop until the number of people who have been assigned a giftee is the same number as the total people
+while (len(hasGiven)!=(len(people))): #loop until all people have been assigned a giftee
+    i+=1                              # iteration counter
+    giver = random.choice(people)     # pick random dude for gifter
+    receiver = giver                  # assign same dude for receiving gift
 
-    receiver = giver = random.choice(people) # set receiver and giver to same person and then
-    while receiver == giver:                 # loop until they are different
+    while (receiver == giver):        # continue picking receiver until the 2 dudes are different dudes
         receiver = random.choice(people)
-        i+=1
-        if (giver != receiver) and (giver not in hasGiven) and (receiver not in hasReceived): #if dudes have not already been giver and receiver respectively
-            pairs.append(giver+'--->'+receiver)             # add 'giver->receiver' to pairs
-            hasGiven.append(giver)                          # add giver to gifters list
-            hasReceived.append(receiver)                    # add receiver to giftees list
-            break
-        else:
-            continue
+
+    if ((receiver in hasReceived) or (giver in hasGiven)): # if the 2 dudes have already been picked for their respective role, start again
+        continue
+
+    pairs.append([giver, receiver]) # write the couple in list
+    hasGiven.append(giver) # write down that gifter dude is confirmed as gifter
+    hasReceived.append(receiver) # write down that receiving dude is confirmed as receiver
+
+    if (len(hasGiven)==len(people)-1): # when only one dude is left to pick from hat, do this check
+        hasGiven.sort() # i need this to compare lists..
+        hasReceived.sort() # i need this to compare lists..
+        if(hasReceived==hasGiven): # if final dude picks himself from hat, then tough shit:
+            hasGiven, hasReceived, pairs = [], [], [] # all dudes have to start again
 
 print('printing pairs:')
 for couple in range(len(pairs)):
